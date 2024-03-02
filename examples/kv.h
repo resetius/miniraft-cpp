6cb4b406177fe88fad5c1919f1c6e9ac1f9daf19
Alexey Ozeritskiy <aozeritsky@gmail.com>
2024-03-02 22:42:42 +0300

    Start work on kv example (#6)

---
+++ examples/kv.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+
+#include <raft.h>
+
+class TKv: public IRsm {
+public:
+    TMessageHolder<TMessage> Read(TMessageHolder<TCommandRequest> message) override;
+    void Write(TMessageHolder<TLogEntry> message) override;
+    TMessageHolder<TLogEntry> Prepare(TMessageHolder<TCommandRequest> message, uint64_t term) override;
+
+private:
+    std::unordered_map<std::string, std::string> H;
+};
